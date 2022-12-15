d73dd2462c4569cdd1cb14d095f55949b3bd62b9
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-12-15 22:41:34 +0530

    App component

--- components/App.jsx
+++ components/App.jsx
@@ -1,1 +1,28 @@
+import React from "react";
+import Footer from "./footer";
+import Header from "./header";
+import Note from "./note";
+import notes from "../notes";
 
+
+
+function App() {
+    return <div>
+        <Header />
+        {/* <Note 
+            title = "This is the title"
+            content = "This is the contents"
+        /> */}
+
+        {notes.map((noteItem) => (<Note
+            key={noteItem.key}
+            title={noteItem.title}
+            content={noteItem.content}
+        />
+        ))};
+
+        <Footer />
+    </div>
+}
+
+export default App;
